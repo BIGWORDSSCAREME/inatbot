e3e45fd24d33745f0f45f7addff2790835bc1583
cbass <rsehlke60@gmail.com>
2023-02-28 09:36:35 -0600

    added importing lists and fixed some stuff

--- inatbot.py
+++ inatbot.py
@@ -1,9 +1,18 @@
-import shutil
 import random
 import requests as r
 import tkinter as tk
+from tkinter import filedialog
 from os import remove
 from PIL import Image, ImageTk
+from json import loads
+
+
+"""THINGS TO DO--
+ALLOW IMPORTABLE JSONS.
+CHANGE requests.json() to python json.loads(). json.loads() is prob safer.
+COMMENT.
+CLEAN UP THE READYGAMESCREEN SECTION. SPECIFCALLY CLICKABLE TEXT AND THE FUNCTIONS. CONFUSING.
+"""
 
 random.seed()
 iNatUrl = "https://api.inaturalist.org/v1/observations?photos=true&taxon_name={}&identifications=most_agree&quality_grade=research&locale=en-US&page=1&per_page=10&order=desc&order_by=created_at"
@@ -69,6 +78,7 @@ class MainMenu(Screen):
 class ReadyGame(Screen):
 
 	deletedSpeciesStack = []
+	#species list is made of tuples of 2. the first item is the name, the second is additional information.
 	speciesList = []
 	gameFrame = None
 	
@@ -78,28 +88,35 @@ class ReadyGame(Screen):
 		self.startGameButton = tk.Button(self.mainFrame, text = "Start", command = self.StartGame)
 		self.startGameButton.pack()
 		self.enterSpecies = tk.Entry(self.mainFrame)
-		self.enterSpecies.bind("<KeyPress>", self.AddSpecies)
+		self.enterSpecies.bind("<KeyPress>", self.SpeciesEntered)
 		self.enterSpecies.pack()
+		self.importSpecies = tk.Button(self.mainFrame, text = "Import from file", command = self.ImportSpeciesFromFile)
+		self.importSpecies.pack()
 
 		self.enteredSpeciesFrame = tk.Frame(self.mainFrame, height = 200)
 		self.deletedSpeciesStack = []
 		self.enteredSpeciesFrame.pack()
 		BindTree(self.mainFrame, "<Button-3>", self.RecoverDeletedSpecies)
 
-	def AddSpecies(self, event):
+	def SpeciesEntered(self, event):
+		#Function for when the enter key is pressed in the enterspecies entry.
 		if event.keysym == "Return":
 			entered = ""
 			entered = self.enterSpecies.get()
-			newSpecies = SpeciesListItem(entered, self.enteredSpeciesFrame, self.deletedSpeciesStack, self.speciesList)
-			self.speciesList.append(entered)
-			newSpecies.BindRecoverDeletedSpecies(self.RecoverDeletedSpecies)
+			self.AddSpecies(entered)
 			self.enterSpecies.delete(0, len(entered))
+
+	def AddSpecies(self, species, info = ""):
+		newSpecies = SpeciesListItem((species, info), self.enteredSpeciesFrame, self.deletedSpeciesStack, self.speciesList)
+		
+		self.speciesList.append((species, info))
+		newSpecies.BindRecoverDeletedSpecies(self.RecoverDeletedSpecies)
 
 	def RecoverDeletedSpecies(self, event):
 		if len(self.deletedSpeciesStack) > 0:
 			species = self.deletedSpeciesStack.pop()
 			species.Reappear()
-			self.speciesList.append(species.name)
+			self.speciesList.append(species.info)
 
 	def StartGame(self):
 		for i in self.deletedSpeciesStack:
@@ -107,26 +124,39 @@ class ReadyGame(Screen):
 		del self.deletedSpeciesStack[:]
 		self.UnpackSelf()
 		self.gameFrame.PackSelf(self.speciesList)
-		
+
+	def ImportSpeciesFromFile(self):
+		#Function for the import button. File dialog asking for a file. The file is read
+		#and all the species are added.
+		fileName = filedialog.askopenfilename(title = "select a file", filetypes = ( ('text files', '*.txt'), ('All files', '*.*') ))
+		fileContent = ""
+		with open(fileName, "r") as file:
+			fileContent = file.read()
+		fileContent = loads(fileContent)
+		for i in fileContent:
+			#you gotta format the json correctly! so it can be imported like this!
+			self.AddSpecies(i["name"], info = i["info"])
+			
 
 class SpeciesListItem:
 
-	name = ""
+	info = None
 	dStack = None
-	sList = None
+	speciesList = None
 
-	def __init__(self, text, parent, delstack, specieslist):
-		self.cableText = tk.Label(parent, text = text, cursor = "man")
+	def __init__(self, info, parent, delstack, specieslist):
+		#Info is a tuple of data in the species list. (NAME, INFORMATION). Setting the label text to the name.
+		self.cableText = tk.Label(parent, text = info[0], cursor = "man")
 		self.cableText.bind("<Button-1>", self.LeftClicked)
 		self.cableText.pack()
 		self.dStack = delstack
-		self.name = text
-		self.sList = specieslist
+		self.info = info
+		self.speciesList = specieslist
 		
 	def LeftClicked(self, event):
 		self.dStack.append(self)
 		self.cableText.pack_forget()
-		self.sList.remove(self.name)
+		self.speciesList.remove(self.info)
 		if len(self.dStack) > 15:
 			self.dStack[0].cableText.destroy()
 			del self.dStack[0]
@@ -141,83 +171,98 @@ class Game(Screen):
 
 	imageList = []
 	speciesList = []
+	#speciesDict keeps track of all the species and URLS to images of them.
 	speciesDict = {}
 	randomizedList = []
 	currentImage = 0
+	points = 0
 		
 	#make a list with the order of all the urls. just go through that loading only a few at a time
 
 	def __init__(self, window):
 		self.mainFrame = tk.Frame(window)
 		self.speciesName = tk.Label(self.mainFrame, text = "Which species?")
+		self.speciesInfo = tk.Label(self.mainFrame, text = "")
 		self.imageCanvas = tk.Canvas(self.mainFrame)
 		self.showNameButton = tk.Button(self.mainFrame, text = "Show name", command = self.ShowName)
+		self.guessSpeciesEntry = tk.Entry(self.mainFrame)
+		self.guessSpeciesEntry.bind("<KeyPress>", self.GuessSpecies)
+		self.speciesGuessResult = tk.Label(self.mainFrame, text = "Enter the species name")
+		self.pointsLabel = tk.Label(self.mainFrame, text = "0 points")
 		self.previousSpeciesButton = tk.Button(self.mainFrame, text = "Previous species", command = lambda: self.NextImage(-1))
 		self.nextSpeciesButton = tk.Button(self.mainFrame, text = "Next species", command = lambda: self.NextImage(1))
 
-	def PackSelf(self, species):
+	def PackSelf(self, speciesList):
 		#ill fuck with the exact positioning later, once it actually works.
-		self.speciesList = species
+		self.speciesList = speciesList
 		self.MakeOrder()
 		self.speciesName.grid(row = 0, column = 0, sticky=tk.N+tk.E+tk.S+tk.W)
+		self.pointsLabel.grid(row = 0, column = 1, sticky=tk.N+tk.E)
 		self.imageCanvas.grid(row = 1, column = 0, sticky=tk.N+tk.E+tk.S+tk.W)
 		self.showNameButton.grid(row = 2, column = 0, sticky=tk.N+tk.E+tk.S+tk.W)
-		self.previousSpeciesButton.grid(row = 3, column = 0, sticky=tk.N+tk.E+tk.S+tk.W)
-		self.nextSpeciesButton.grid(row = 3, column = 1, sticky=tk.N+tk.E+tk.S+tk.W)
+		self.guessSpeciesEntry.grid(row = 3, column = 0, sticky=tk.N+tk.E+tk.S+tk.W)
+		self.speciesGuessResult.grid(row = 3, column = 1, sticky=tk.N+tk.E+tk.S+tk.W)
+		self.previousSpeciesButton.grid(row = 4, column = 0, sticky=tk.N+tk.E+tk.S+tk.W)
+		self.nextSpeciesButton.grid(row = 4, column = 1, sticky=tk.N+tk.E+tk.S+tk.W)
+		self.speciesInfo.grid(row = 5, column = 0, sticky=tk.N+tk.E+tk.S+tk.W)
 		self.mainFrame.pack()
-		self.PickImage()
+		#can i delete this function? I think i can but ill test it after everything else is working
 		imageDownload = self.DownloadImage(self.randomizedList[1]["url"])
-		self.randomizedList[1]["image"] = imageDownload[0]
-		self.randomizedList[1]["filename"] = imageDownload[1]
+		self.randomizedList[1]["image"] = imageDownload
 		self.NextImage(1)
 
 	def MakeOrder(self):
 		for species in self.speciesList:
-			speciesUrl = iNatUrl.format(species)
-			jsonData = r.get(speciesUrl).json()
+			#Species[0] is the name. Species[1] is the data.
+			speciesUrl = iNatUrl.format(species[0])
+			jsonData = loads(r.get(speciesUrl).content)
 			if jsonData['total_results'] == 0:
-				print(species + " has no results.")
-			self.speciesDict[species] = []
+				print(species[0] + " has no results.")
+			self.speciesDict[species[0]] = []
 			for i in jsonData["results"]:
-				self.speciesDict[species].append(i["photos"][0]["url"])
-				self.randomizedList.append({"species": species, "url": i["photos"][0]["url"] })
+				#speciesDict gets the URLS to images of the species. {species1: [url1, url2...], species2: [url1, url2...]}
+				self.speciesDict[species[0]].append(i["photos"][0]["url"])
+				self.randomizedList.append({"species": species[0], "info": species[1], "url": i["photos"][0]["url"] })
 		random.shuffle(self.randomizedList)
 
 	def DownloadImage(self, url):
+		#The url looks like this
+		#https://inaturalist-open-data.s3.amazonaws.com/photos/257918386/square.jpeg
+		#The following lines replace the square with medium. INaturalist has a few
+		#Different sizes of their images - square, small, medium, large. Square is what
+		#is given from the api call from before, but that's like super low res.
 		index = url.find("square")
-		idIndex = url.find("/photos/")
-		id = url[idIndex + 8: idIndex + 17]
 		url2 = url[:index]
 		url2 = url2 + "medium"
 		fileExtension = url[index + 6:]
 		url = url2 + fileExtension
 		response = r.get(url, stream = True)
-		#in the url replace the "square" with medium to get a larger file.
 		if response.status_code == 200:
-			with open(id + fileExtension, 'wb') as f: # this might be failing because it does not have a file extension
-				shutil.copyfileobj(response.raw, f)
-			img = Image.open(id + fileExtension)
+			#getting the image and turning it into a PIL image to be displayed.
+			img = Image.open(response.raw)
 			img = ImageTk.PhotoImage(img)
-			#return the image and the name of the file
-			return (img, id + fileExtension)
+			return img
 		else:
 			print("failed to grab image with status code " + str(response.status_code))
-			print(iNatPicUrl.format(id))
 			return None
 
 	def NextImage(self, increment):
 		self.speciesName.config(text = "Which species?")
+		self.speciesInfo.config(text = "")
+		self.guessSpeciesEntry.delete(0, len(self.guessSpeciesEntry.get()))
+		self.speciesGuessResult.config(text = "Enter the species name")
 		self.currentImage += increment
 		index = self.currentImage % len(self.randomizedList)
 		self.LoadImage(self.randomizedList[index]["image"])
 		
 		nextImage = self.randomizedList[(self.currentImage + increment) % len(self.randomizedList)]
-		self.DownloadImage(nextImage["url"])
 		imageDownload = self.DownloadImage(nextImage["url"])
-		nextImage["image"] = imageDownload[0]
-		nextImage["filename"] = imageDownload[1]
+		nextImage["image"] = imageDownload
 		
 		previousImage = self.randomizedList[(self.currentImage - (2 * increment)) % len(self.randomizedList)]
+		
+
+		#THIS STUFF NEEDS TO BE CHANGED. NO LONGER USING IMAGES STORED AS FILES!!
 		if "filename" in previousImage:
 		#The above line checks to see if there is a filename field in the KEYS of the
 		#dictionary, previousImage. Weird.
@@ -230,29 +275,23 @@ class Game(Screen):
 		self.imageCanvas.create_image(0, 0, anchor = tk.NW, image = image)
 		self.imageCanvas.image = image
 
-	def PickImage(self):
-		#Pick randomly from the list of species and stuff.
-		#If there are no species, rangeint will fail. 0 to -1 and the number on the right has to be higher.
-		species = self.speciesList[random.randint(0, len(self.speciesList) - 1)]
-		url = self.speciesDict[species][random.randint(0, len(self.speciesDict[species]) - 1)]
-		self.speciesDict[species].remove(url)
-		if len(self.speciesDict[species]) == 0:
-			speciesList.remove(species)
-		#Adds to the end of the list
-		self.imageList.append(self.DownloadImage(url))
-
 	def ShowName(self):
 		self.speciesName.config(text = self.randomizedList[self.currentImage % len(self.randomizedList)]["species"])
+		if "info" in self.randomizedList[self.currentImage % len(self.randomizedList)]:
+			self.speciesInfo.config(text = self.randomizedList[self.currentImage % len(self.randomizedList)]["info"])
 
-	def __del__(self):
-		for i in self.randomizedList:
-			if "filename" in i:
-				#might want to change it from removing it based on the path string
-				#to something else. not a big deal but if the id string somehow contained
-				#a file path it could delete your files good heavens.
-				#Also this fails to delete one of the images. Not sure why. something to fix.
-				try:
-					remove(path = i["filename"])
-				except: pass
+	def GuessSpecies(self, event):
+		#Event fired when enter is pressed in the "guess the species" entry.
+		if event.keysym == "Return":
+			entered = self.guessSpeciesEntry.get()
+			if entered.upper() == self.randomizedList[self.currentImage % len(self.randomizedList)]["species"].upper():
+				#Check if its the name of the current species
+				self.points += 1
+				self.speciesGuessResult.config(text = "Correct!")
+			else:
+				self.points -= 1
+				self.speciesGuessResult.config(text = "Inorrect :(")
+			self.pointsLabel.config(text = str(self.points) + " points")
+				
 
 mainWindow = AppWindow()

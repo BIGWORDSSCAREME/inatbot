7d824c918309143363b2e9f7135402c08b036cfd
cbass <rsehlke60@gmail.com>
2023-03-07 16:05:01 -0600

    Style changes

--- inatbot.py
+++ inatbot.py
@@ -122,25 +122,27 @@ class ReadyGame(Screen):
 		#GameFrame is the next screen - the screen of the game. for packing + unpacking this screen.
 		self.gameFrame = game
 		self.mainFrame = Frame(window)
+		self.instructionLabel = Label(self.mainFrame, text = "Type a species name (e.g Viola sororia) then press enter to add it to the list.\nCheck your spelling!\nAlso check (out) my venmo lol @Sebastian-Ehlke", justify = "center")
+		self.instructionLabel.grid(column = 1, row = 0)
 		self.startGameButton = Button(self.mainFrame, text = "Start", command = self.StartGame)
-		self.startGameButton.grid(column = 1, row = 0, columnspan = 2)
+		self.startGameButton.grid(column = 1, row = 1, columnspan = 2)
 		self.enterSpecies = tk.Entry(self.mainFrame)
 		self.enterSpecies.bind("<KeyPress>", self.SpeciesEntered)
-		self.enterSpecies.grid(column = 1, row = 1, columnspan = 2)
+		self.enterSpecies.grid(column = 1, row = 2, columnspan = 2)
 		self.importSpecies = Button(self.mainFrame, text = "Import from file", command = self.ImportSpeciesFromFile)
-		self.importSpecies.grid(column = 1, row = 2, columnspan = 2)
+		self.importSpecies.grid(column = 1, row = 3, columnspan = 2)
 		
 
 		#Using tk.Frame instead of the Frame (which would be ttk.Frame) because I want a border
 		#Using a tk.Frame is probably the reason its a different color than the rest of the background.
 		#the ttk Frames have a styled/colored background. I like how it looks though.
 		self.canvasBorderFrame = tk.Frame(self.mainFrame, highlightbackground = "grey", highlightthickness = 2)
-		self.canvasBorderFrame.grid(column = 1, row = 3)
+		self.canvasBorderFrame.grid(column = 1, row = 4)
 		#tk.Canvas uses the tkinter canvas. Specifying nothing before a widget uses the stylable ttk widget.
 		self.enteredSpeciesCanvas = tk.Canvas(self.canvasBorderFrame)
 		self.enteredSpeciesCanvas.pack(anchor = "center")
 		self.enteredSpeciesScrollbar = Scrollbar(self.mainFrame, orient = "vertical")
-		self.enteredSpeciesScrollbar.grid(column = 2, row = 3, sticky = tk.N + tk.S)
+		self.enteredSpeciesScrollbar.grid(column = 2, row = 4, sticky = tk.N + tk.S)
 		self.enteredSpeciesCanvas.configure(yscrollcommand = self.enteredSpeciesScrollbar.set)
 		self.enteredSpeciesCanvas.bind("<Configure>", lambda e: self.enteredSpeciesCanvas.configure(scrollregion = self.enteredSpeciesCanvas.bbox("all")))
 		self.enteredSpeciesFrame = Frame(self.enteredSpeciesCanvas)
@@ -149,6 +151,12 @@ class ReadyGame(Screen):
 		self.enteredSpeciesScrollbar.config(command = self.enteredSpeciesCanvas.yview)
 		self.deletedSpeciesStack = []
 		BindTree(self.mainFrame, "<Button-3>", self.RecoverDeletedSpecies)
+		
+		self.helpLabel = Label(self.mainFrame, justify = "center", text = "Click on a species to remove it from the list\nRight click to recover the last deleted species.")
+		self.helpLabel.grid(column = 1, row = 5)
+
+		#Column configure makes the specified columns (0 and 3) have a different weight.
+		#Weight makes them fill space basically, so it pushes the things in between 0 and 3 to the center
 		self.mainFrame.grid_columnconfigure((0, 3), weight = 1)
 
 	def ResetScrollregion(self, event):
@@ -177,6 +185,8 @@ class ReadyGame(Screen):
 			self.speciesList.append(species)
 
 	def StartGame(self):
+		if len(self.speciesList) == 0:
+			return
 		for i in self.deletedSpeciesStack:
 			i.cableText.destroy()
 		del self.deletedSpeciesStack[:]
@@ -214,7 +224,7 @@ class SpeciesListItem:
 
 	def __init__(self, info, parent, delstack, specieslist):
 		#Info is a tuple of data in the species list. (NAME, INFORMATION). Setting the label text to the name.
-		self.cableText = Label(parent, text = info["name"], cursor = "man")
+		self.cableText = Label(parent, text = info["name"], cursor = "pirate", justify = "center")
 		self.cableText.bind("<Button-1>", self.LeftClicked)
 		self.cableText.pack()
 		self.dStack = delstack
@@ -250,7 +260,25 @@ class Game(Screen):
 	def __init__(self, window):
 		self.mainFrame = Frame(window)
 		self.speciesName = Label(self.mainFrame, text = "Which species?")
-		self.speciesInfo = Label(self.mainFrame, text = "")
+
+		self.speciesInfoFrame = Frame(self.mainFrame)
+		self.speciesInfoScrollbar = Scrollbar(self.speciesInfoFrame, orient = "vertical")
+		#bd -2 gets rid of the canvas border which is set at 2 pixels automatically
+		self.speciesInfoCanvas = tk.Canvas(self.speciesInfoFrame, height = 50, bd = -2)
+		self.speciesInfoScrollingFrame = Frame(self.speciesInfoCanvas, height = 50)
+		self.speciesInfoScrollingFrame.bind("<Configure>", self.ResetScrollregion)
+		self.speciesInfo = Label(self.speciesInfoScrollingFrame, text = "", justify = "center")
+		self.speciesInfoCanvas.configure(yscrollcommand = self.speciesInfoScrollbar.set)
+		self.speciesInfoCanvas.bind("<Configure>", lambda e: self.speciesInfoCanvas.configure(scrollregion = self.speciesInfoCanvas.bbox("all")))
+		self.speciesInfoScrollbar.config(command = self.speciesInfoCanvas.yview)
+		self.speciesInfoCanvas.grid(column = 1, row = 0)
+		self.speciesInfoScrollbar.grid(column = 2, row = 0, sticky = tk.N + tk.S)
+		self.speciesInfoFrame.grid_columnconfigure((0, 3), weight = 1)
+		self.speciesInfo.pack()
+		#Below line makes it so the frame is the size of the canvas, fills the canvas
+		self.speciesInfoCanvasFrame = self.speciesInfoCanvasFrame = self.speciesInfoCanvas.create_window((0, 0), window = self.speciesInfoScrollingFrame, anchor = "n")
+		self.speciesInfoCanvas.bind("<Configure>", self.FrameWidth)
+
 		self.imageCanvas = tk.Canvas(self.mainFrame, height = 400, background = "black")
 		self.showNameButton = Button(self.mainFrame, text = "Show name", command = self.ShowName)
 		self.guessSpeciesEntry = Entry(self.mainFrame)
@@ -261,7 +289,6 @@ class Game(Screen):
 		self.nextSpeciesButton = Button(self.mainFrame, text = "Next species", command = lambda: self.NextImage(1))
 		self.exitButton = Button(self.mainFrame, text = "Back to enter species", command = self.BackButton)
 
-		
 		self.speciesName.grid(row = 0, column = 1, columnspan = 2)
 		self.pointsLabel.grid(row = 0, column = 2, sticky = tk.N + tk.E, columnspan = 2, padx = (0, 100))
 		self.imageCanvas.grid(row = 1, column = 1, columnspan = 2)
@@ -270,11 +297,16 @@ class Game(Screen):
 		self.speciesGuessResult.grid(row = 3, column = 2, sticky = tk.W)
 		self.previousSpeciesButton.grid(row = 4, column = 1, sticky = tk.E)
 		self.nextSpeciesButton.grid(row = 4, column = 2, sticky = tk.W)
-		self.speciesInfo.grid(row = 5, column = 1, columnspan = 2)
-		self.exitButton.grid(row = 6, column = 0, sticky = tk.W, columnspan = 2)
+		self.exitButton.grid(row = 6, column = 0, sticky = tk.W + tk.S, columnspan = 2, padx = (50, 0), pady = (0, 25))
+		
+		#Row configure/column configuremake these columns/rows fill space
 		self.mainFrame.grid_columnconfigure((0, 3), weight = 1)
+		self.mainFrame.grid_rowconfigure(6, weight = 1)
 
 	def PackSelf(self, speciesList, readygamescreen):
+
+		self.speciesInfoCanvas.configure(width = self.speciesInfoCanvas.winfo_width() - self.speciesInfoScrollbar.winfo_width())
+
 		self.readyGameScreen = readygamescreen
 		self.speciesList = speciesList
 		self.MakeOrder()
@@ -287,6 +319,13 @@ class Game(Screen):
 		imageDownload = self.DownloadImage(self.speciesDict[self.randomizedList[1]["name"]][self.randomizedList[1]["index"]]["url"])
 		self.speciesDict[self.randomizedList[1]["name"]][self.randomizedList[1]["index"]]["image"] = imageDownload
 		self.NextImage(1)
+
+	def ResetScrollregion(self, event):
+		self.speciesInfoCanvas.configure(scrollregion = self.speciesInfoCanvas.bbox("all"))
+
+	def FrameWidth(self, event):
+		canvasWidth = event.width
+		self.speciesInfoCanvas.itemconfig(self.speciesInfoCanvasFrame, width = canvasWidth)
 
 	def BackButton(self):
 		self.UnpackSelf()
@@ -353,8 +392,9 @@ class Game(Screen):
 			return None
 
 	def NextImage(self, increment):
+		self.speciesInfoFrame.grid_forget()
 		self.speciesName.config(text = "Which species?")
-		self.speciesInfo.config(text = "")
+		self.speciesInfo.config(text = "", justify = "center")
 		self.guessSpeciesEntry.delete(0, len(self.guessSpeciesEntry.get()))
 		self.speciesGuessResult.config(text = "Enter the species name")
 		self.currentImage += increment
@@ -403,6 +443,7 @@ class Game(Screen):
 		if len(species["info"]) > 0:
 			#If there is information about the species entered, it will show up on screen.
 			self.speciesInfo.config(text = species["info"])
+			self.speciesInfoFrame.grid(row = 5, column = 1, columnspan = 2)
 
 	def GuessSpecies(self, event):
 		#Event fired when enter is pressed in the "guess the species" entry.
